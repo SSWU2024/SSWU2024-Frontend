6f042a099e6f5422c1ac60dd883e87f0a59f626c
seoAreum <1971236@hansung.ac.kr>
2024-09-20 01:49:56 +0900

    Revert "chore: 라우터 이름 변경"
    This reverts commit 85e28bb02ee0d24fa7bae23afb58290b214faf24.

--- src/DesktopRouter.tsx
+++ src/DesktopRouter.tsx
@@ -2,13 +2,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import DesignersPage from './desktop/Designers/pages/DesignersPage';
 import DisplayPages from './desktop/Displays/pages/DisplayPages';
 import MainPage from './desktop/Main/pages/MainPage';
-import Studios from './desktop/Studios/components/Studios';
+import WorksPage from './desktop/Works/pages/WorksPage';
 
 const router = createBrowserRouter([
   { path: '/', element: <MainPage /> },
-  { path: '/studio', element: <Studios /> },
-  { path: '/designer', element: <DesignersPage /> },
-  { path: '/display', element: <DisplayPages /> },
+  { path: '/works', element: <WorksPage /> },
+  { path: '/designers', element: <DesignersPage /> },
+  { path: '/displays', element: <DisplayPages /> },
 ]);
 
 const DesktopRouter = () => {
