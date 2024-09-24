607686ebf5780cea061d5ce53d6ac0d59aac5d9f
seoAreum <1971236@hansung.ac.kr>
2024-09-24 23:28:23 +0900

    feat: 컴포넌트 분리

--- src/desktop/Displays/pages/DisplayPages.tsx
+++ src/desktop/Displays/pages/DisplayPages.tsx
@@ -1,4 +1,6 @@
 import { css } from '@emotion/react';
+import DisplayImages from '../\bcomponents/DisplayImages';
+import DisplayInfo from '../\bcomponents/DisplayInfo';
 import { colors, fonts } from '../../../styles/theme';
 import PageLayout from '../../Common/PageLayout';
 import { ImgBg2Web } from '../../assets/image';
@@ -69,47 +71,11 @@ const DisplayPages = () => {
         <img src={ImgBg2Web} css={bg} />
         {displays.map((display) => {
           const { name, studioList, images } = display;
-          const firstColomnImages = images.slice(0, 3);
-          const secondColumnImages = images.slice(3, 5);
-          const thirdColumnImages = images.slice(5, 7);
 
           return (
             <article key={name} css={displayContainer}>
-              <p css={displayName}>{name}</p>
-              <div css={studiosContainer}>
-                {studioList?.map((studio, idx) => {
-                  const lastStudio = idx === studioList.length - 1;
-                  return (
-                    <p key={studio} css={studioName}>
-                      {studio}
-                      {!lastStudio && ','}
-                    </p>
-                  );
-                })}
-              </div>
-
-              <article css={imgContainer}>
-                <div css={firstImagesContainer}>
-                  {firstColomnImages.map((image, idx) => {
-                    const { imgPath } = image;
-                    return <img key={idx} src={imgPath} css={img} />;
-                  })}
-                </div>
-
-                <div css={secondImagesContainer}>
-                  {secondColumnImages.map((image, idx) => {
-                    const { imgPath } = image;
-                    return <img key={idx} src={imgPath} css={img} />;
-                  })}
-                </div>
-
-                <div css={thirdImagesContainer}>
-                  {thirdColumnImages.map((image, idx) => {
-                    const { imgPath } = image;
-                    return <img key={idx} src={imgPath} css={img} />;
-                  })}
-                </div>
-              </article>
+              <DisplayInfo name={name} studioList={studioList} />
+              <DisplayImages images={images} />
             </article>
           );
         })}
@@ -162,81 +128,4 @@ const studiosContainer = css`
 const studioName = css`
   color: ${colors.gray900};
   ${fonts.desktop_body_reg_20};
-`;
-
-const imgContainer = css`
-  display: flex;
-  gap: calc(100% / 180);
-`;
-
-const firstImagesContainer = css`
-  display: flex;
-  gap: calc(100vh / 101.25);
-  flex-direction: column;
-
-  width: calc(100% / 4);
-
-  img:nth-of-type(1) {
-    min-height: 25.5rem;
-
-    height: calc(100vh / 3.18);
-  }
-
-  img:nth-of-type(2) {
-    min-height: 26.1rem;
-
-    height: calc(100vh / 3.1034);
-  }
-
-  img:nth-of-type(3) {
-    min-height: 27.6rem;
-
-    height: calc(100vh / 2.9348);
-  }
-`;
-
-const secondImagesContainer = css`
-  display: flex;
-  gap: calc(100vh / 101.25);
-  flex-direction: column;
-  min-width: 64.4rem;
-
-  width: calc(100% / 2.05);
-
-  img:nth-of-type(1) {
-    min-height: 46rem;
-
-    height: calc(100vh / 1.7609);
-  }
-
-  img:nth-of-type(2) {
-    min-height: 34rem;
-
-    height: calc(100vh / 2.38);
-  }
-`;
-
-const thirdImagesContainer = css`
-  display: flex;
-  gap: calc(100vh / 101.25);
-  flex-direction: column;
-
-  width: calc(100% / 4);
-
-  img:nth-of-type(1) {
-    min-height: 41rem;
-
-    height: calc(100vh / 1.9756);
-  }
-
-  img:nth-of-type(2) {
-    min-height: 39rem;
-
-    height: calc(100vh / 2.0769);
-  }
-`;
-
-const img = css`
-  width: 100%;
-  object-fit: cover;
 `;
