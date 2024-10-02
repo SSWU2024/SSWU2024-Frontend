0e3a3186b8b6e31313f4fbfcc2270c003ffd1d3d
Doyun Lee <doyn511@ajou.ac.kr>
2024-10-02 21:12:21 +0900

    feat: 태블릿뷰 헤더, 푸터, 페이지레이아웃 퍼블리싱

--- src/tablet/Main/page/MainPage.tsx
+++ src/tablet/Main/page/MainPage.tsx
@@ -1,5 +1,19 @@
-const MainPage = () => {
-  return <div>MainPage</div>;
-};
+import { css } from '@emotion/react';
+import PageLayout from '../../Common/PageLayout';
+
+function MainPage() {
+  return (
+    <PageLayout>
+      <div css={mainContainer}>
+        <p>메인페이지</p>
+      </div>
+    </PageLayout>
+  );
+}
 
 export default MainPage;
+
+const mainContainer = css`
+  width: 100%;
+  padding-top: 5.6rem;
+`;
