9b6c62c867902af29d69e5d61a5b9510bfe29262
Doyun Lee <doyn511@ajou.ac.kr>
2024-10-04 13:10:01 +0900

    chore: 어셋 추가

--- src/mobile/assets/image/index.ts
+++ src/mobile/assets/image/index.ts
@@ -1,1 +1,3 @@
+export { default as ImgBg1Mobile } from './img_bg1_mobile.png';
 export { default as ImgBg2Mobile } from './img_bg2_mobile.png';
+export { default as ImgBg3Mobile } from './img_bg3_mobile.png';
