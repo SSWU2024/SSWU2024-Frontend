4fe2406df6ec8214cd05f0d0c206f35d93b9ec84
Doyun Lee <doyn511@ajou.ac.kr>
2024-10-10 00:55:20 +0900

    chore: dummy 데이터

---
+++ src/mobile/constants/DISPLAY.ts
@@ -0,0 +1,124 @@
+export const DISPLAY = [
+  {
+    displayId: 1,
+    displayNm: '가온전시실',
+    images: [
+      {
+        imgId: 1,
+        imgNm: 'display_image_1',
+        sort: 1,
+        imgPath:
+          'https://i.postimg.cc/JnBwTyWJ/26ba42d439c6ad04bc071202e52044e6.jpg',
+        fileFormat: 'jpg',
+      },
+      {
+        imgId: 2,
+        imgNm: 'display_image_2',
+        sort: 1,
+        imgPath:
+          'https://i.postimg.cc/FsVMn5wP/30d2c26659c3191f659aa61e145737de.jpg',
+        fileFormat: 'jpg',
+      },
+      {
+        imgId: 3,
+        imgNm: 'display_image_3',
+        sort: 2,
+        imgPath:
+          'https://i.postimg.cc/j2zpbG2S/45b664b78adc10b48b4c977c1e828c9a.jpg',
+        fileFormat: 'png',
+      },
+      {
+        imgId: 4,
+        imgNm: 'display_image_4',
+        sort: 2,
+        imgPath:
+          'https://i.postimg.cc/ZYptpyHz/702c826f56dbe016c37d8d5c6705a082.jpg',
+        fileFormat: 'jpg',
+      },
+      {
+        imgId: 5,
+        imgNm: 'display_image_5',
+        sort: 3,
+        imgPath:
+          'https://i.postimg.cc/ZYptpyHz/702c826f56dbe016c37d8d5c6705a082.jpg',
+        fileFormat: 'png',
+      },
+      {
+        imgId: 6,
+        imgNm: 'display_image_6',
+        sort: 3,
+        imgPath:
+          'https://i.postimg.cc/4NGC5N27/8328e52e463c8097d9e977b36547076e.jpg',
+        fileFormat: 'jpg',
+      },
+      {
+        imgId: 7,
+        imgNm: 'display_image_7',
+        sort: 4,
+        imgPath:
+          'https://i.postimg.cc/t4RLKvj9/fdc0f223c7a2575c9197defaa6cdf190.jpg',
+        fileFormat: 'jpeg',
+      },
+    ],
+  },
+  {
+    displayId: 2,
+    displayNm: '파이룸',
+    images: [
+      {
+        imgId: 1,
+        imgNm: 'display_image_1',
+        sort: 1,
+        imgPath: 'https://i.postimg.cc/5ychDxxr/IMG-2398.jpg',
+        fileFormat: 'jpg',
+      },
+      {
+        imgId: 2,
+        imgNm: 'display_image_2',
+        sort: 2,
+        imgPath:
+          'https://i.postimg.cc/Qd6Rhg81/Kakao-Talk-Photo-2024-04-10-01-03-05.jpg',
+        fileFormat: 'png',
+      },
+      {
+        imgId: 3,
+        imgNm: 'display_image_3',
+        sort: 3,
+        imgPath: 'https://i.postimg.cc/GpfwJF6d/IMG-0414.jpg',
+        fileFormat: 'jpg',
+      },
+      {
+        imgId: 4,
+        imgNm: 'display_image_4',
+        sort: 4,
+        imgPath:
+          'https://i.postimg.cc/JnBwTyWJ/26ba42d439c6ad04bc071202e52044e6.jpg',
+        fileFormat: 'jpeg',
+      },
+      {
+        imgId: 5,
+        imgNm: 'display_image_5',
+        sort: 5,
+        imgPath:
+          'https://i.postimg.cc/FsVMn5wP/30d2c26659c3191f659aa61e145737de.jpg',
+        fileFormat: 'png',
+      },
+      {
+        imgId: 6,
+        imgNm: 'display_image_6',
+        sort: 6,
+        imgPath:
+          'https://i.postimg.cc/ZYptpyHz/702c826f56dbe016c37d8d5c6705a082.jpg',
+        fileFormat: 'jpg',
+      },
+      {
+        imgId: 7,
+        imgNm: 'display_image_7',
+        sort: 7,
+        imgPath:
+          'https://i.postimg.cc/t4RLKvj9/fdc0f223c7a2575c9197defaa6cdf190.jpg',
+        fileFormat: 'jpeg',
+      },
+    ],
+  },
+];
