8930360732aa6966b94457c642e3dd4e51b6dc9d
seoAreum <1971236@hansung.ac.kr>
2024-10-09 15:12:04 +0900

    chore: 더미 데이터 삭제

--- src/desktop/Designer/pages/DesignerPage.tsx
+++ src/desktop/Designer/pages/DesignerPage.tsx
@@ -7,45 +7,6 @@ import PageLayout from '../../Common/PageLayout';
 import DesignerContact from '../components/DesignerContact';
 import DesignerWorks from '../components/DesignerWorks';
 
-const DUMMY = {
-  name: '양종욱',
-  engName: 'Jonguk Yang',
-  major: 'Computer Science',
-  email: 'didwhddnr123@naver.com',
-  instagram: '@jong_uks',
-  behance: 'https://juclass.co.kr',
-  works: [
-    {
-      url: 'title-1',
-      workTitle: '디지털 일러스트',
-      studioNm: '시각디자인스튜디오 A',
-      images: [
-        {
-          imgPath: 'https://xen-api.linkareer.com/attachments/80334',
-          fileFormat: 'jpg',
-        },
-        {
-          imgPath:
-            'https://i.pinimg.com/originals/a0/89/e7/a089e759d7e713b4eba7b6cda87b6c8a.gif',
-          fileFormat: 'gif',
-        },
-      ],
-    },
-    {
-      url: 'title-2',
-      workTitle: '서비스 디자인',
-      studioNm: '디자인씽킹스튜디오',
-      images: [
-        {
-          imgPath:
-            'https://i.pinimg.com/236x/13/26/c1/1326c1f3ec2a54bfc0893a0c582360de.jpg',
-          fileFormat: 'jpg',
-        },
-      ],
-    },
-  ],
-};
-
 const DesignerPage = () => {
   const { designerId } = useLocation().state;
   const { designerDetail, isLoading } = useGetDesignerDetail(designerId);
