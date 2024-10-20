93ac18c029ef34abcb48c032fc6d16f8467285b9
seoAreum <1971236@hansung.ac.kr>
2024-10-20 19:02:22 +0900

    chore: 모바일 모달 삭제

--- src/mobile/Display/page/DisplayPage.tsx
+++ src/mobile/Display/page/DisplayPage.tsx
@@ -1,6 +1,5 @@
 import { css } from '@emotion/react';
-import { useEffect, useRef, useState } from 'react';
-import DisplayModal from '../../../components/DisplayModal';
+import { useEffect, useRef } from 'react';
 import { colors, fonts } from '../../../styles/theme';
 import { ImgBg2Mobile } from '../../assets/image';
 import PageLayout from '../../Common/PageLayout';
@@ -8,9 +7,6 @@ import { DISPLAY } from '../../constants/DISPLAY';
 import { imageType } from '../types/imageType';
 
 const DisplayPage = () => {
-  const [isOpen, setIsOpen] = useState(false); // 모달 open / close 관리
-
-  const selectImg = useRef(0);
   const studio1ImageList = useRef<imageType[]>(); // studio 1 이미지 리스트
   const studio2ImageList = useRef<imageType[]>(); // studio 2 이미지 리스트
 
@@ -25,30 +21,8 @@ const DisplayPage = () => {
   const StudioImages2 =
     DISPLAY.find((item) => item.displayId === 2)?.images || [];
 
-  /** 이미지 클릭 시 모달 oepn */
-  const onClickImgage = (imgId: number) => {
-    selectImg.current = imgId;
-    setIsOpen(true);
-    document.body.style.overflow = '';
-  };
-
-  /** 모달 내 x 버튼 클릭 시 모달 close (props) */
-  const closeModal = () => {
-    setIsOpen(false);
-    document.body.style.overflow = 'hidden';
-  };
-
   return (
     <PageLayout>
-      {isOpen && (
-        <DisplayModal
-          imgId={selectImg.current}
-          studio1ImageList={studio1ImageList.current}
-          studio2ImageList={studio2ImageList.current}
-          closeModal={closeModal}
-        />
-      )}
-
       <section css={displayCss(ImgBg2Mobile)}>
         <div css={studioCss}>
           <div css={textCss}>
@@ -62,14 +36,7 @@ const DisplayPage = () => {
           <ul css={imgCss}>
             {StudioImages1.map((item) => {
               const { imgId, imgPath } = item;
-              return (
-                <img
-                  key={imgId}
-                  src={imgPath}
-                  alt="가온전시실 이미지"
-                  onClick={() => onClickImgage(imgId)}
-                />
-              );
+              return <img key={imgId} src={imgPath} alt="가온전시실 이미지" />;
             })}
           </ul>
         </div>
@@ -82,14 +49,7 @@ const DisplayPage = () => {
           <ul css={imgCss}>
             {StudioImages2.map((item) => {
               const { imgId, imgPath } = item;
-              return (
-                <img
-                  key={imgId}
-                  src={imgPath}
-                  alt="파이룸 이미지"
-                  onClick={() => onClickImgage(imgId)}
-                />
-              );
+              return <img key={imgId} src={imgPath} alt="파이룸 이미지" />;
             })}
           </ul>
         </div>
