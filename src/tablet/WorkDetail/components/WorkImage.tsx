5bad243ce73e46609950bcf2cf4029eaf113e0bc
Doyun Lee <doyn511@ajou.ac.kr>
2024-10-30 02:23:56 +0900

    feat: 비디오 링크 연결

--- src/tablet/WorkDetail/components/WorkImage.tsx
+++ src/tablet/WorkDetail/components/WorkImage.tsx
@@ -1,14 +1,40 @@
 import { css } from '@emotion/react';
+import React from 'react';
 import { WorkImageProps } from '../../../mobile/WorkDetail/types/WorkDetailType';
 
 const WorkImage = ({ images }: WorkImageProps) => {
   return (
     <div css={imageSection}>
       {images &&
-        images.map((item) => {
-          const { imgPath } = item;
+        images.map((image) => {
+          const { sort, imgPath, fileFormat } = image;
+          const isVideo = fileFormat === 'video';
+          const splitedUrl = imgPath.split('/').pop();
+          const isYoutubeUrl = splitedUrl?.includes('watch');
+          const videoId =
+            isVideo &&
+            (isYoutubeUrl
+              ? splitedUrl?.split('?')[1].split('=')[1]
+              : splitedUrl?.split('?')[0]);
 
-          return <img src={imgPath} key={imgPath} css={imageCss} />;
+          return (
+            <React.Fragment key={sort + imgPath}>
+              {isVideo ? (
+                <div css={videoContainer}>
+                  <iframe
+                    css={workVideo}
+                    src={
+                      isYoutubeUrl
+                        ? `https://www.youtube.com/embed/${videoId}`
+                        : `https://player.vimeo.com/video/${videoId}`
+                    }
+                  />
+                </div>
+              ) : (
+                <img src={imgPath} css={imageCss} />
+              )}
+            </React.Fragment>
+          );
         })}
     </div>
   );
@@ -26,4 +52,22 @@ const imageSection = css`
 
 const imageCss = css`
   width: 100%;
+`;
+
+const videoContainer = css`
+  position: relative;
+
+  /* 16:9 비율 */
+  padding-top: 56.25%;
+`;
+
+const workVideo = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+
+  border: none;
 `;
